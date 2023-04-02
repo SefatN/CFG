14aba5a89db8ed8b4705446938448be8f0d4d973
SefatN <128522397+SefatN@users.noreply.github.com>
2023-04-03 03:08:26 +0600

    updated shop.py  into smaller blocks
    edited shop.py so the unit tests can be run.

--- shop.py
+++ shop.py
@@ -5,47 +5,75 @@ class InsufficientFundsError(Exception):
     pass
 
 
-items = {"bread": 50, "chips": 80, "milk": 120}
-customer_money = 100
-
-print(colored('Welcome to our shop!', 'cyan'))
-while True:
+def display_items(items):
     print("Here are the available items:- ")
     for item, price in items.items():
         print(f"{item}: £{price}")
-    print("Please type 'exit' to leave the shop.")
 
-    try:
+
+def get_user_choice(items):
+    while True:
         choice = input("What would you like to buy?:- ")
         if choice == "exit":
             print("Goodbye!")
-            break
+            exit()
         elif choice not in items:
-            raise ValueError("Invalid choice!")
-        elif customer_money >= items[choice]:
-            print(f"Here's your {choice}!")
-            customer_money -= items[choice]
+            print("Invalid choice! Please choose again.")
         else:
-            attempts = 0
-            while attempts < 3:
-                additional_money = input("You do not have enough money. Do you have more? If so, how much? Type 0 to exit. ")
-                if additional_money == '0':
-                    print("Goodbye!")
-                    exit()
-                additional_money = int(additional_money)
-                customer_money += additional_money
-                if customer_money >= items[choice]:
-                    print(f"Here's your {choice}!")
-                    customer_money -= items[choice]
-                    break
-                else:
-                    attempts += 1
-            else:
-                raise InsufficientFundsError("You do not have enough money for this item. Goodbye!")
-    except ValueError as ve:
-        print(ve)
-    except InsufficientFundsError as ife:
-        print(ife)
-        break
+            return choice
+
+
+def get_additional_money():
+    while True:
+        additional_money = input("You do not have enough money. Do you have more? If so, how much? Type 0 to exit. ")
+        if additional_money == '0':
+            print("Goodbye!")
+            exit()
+        try:
+            additional_money = int(additional_money)
+            return additional_money
+        except ValueError:
+            print("Invalid input! Please enter a valid amount.")
+
+
+def buy_item(choice, customer_money, items):
+    if customer_money >= items[choice]:
+        print(f"Here's your {choice}!")
+        customer_money -= items[choice]
+        return customer_money, 0
     else:
-        print(f"You have £{customer_money} left.")`
+        attempts = 0
+        while attempts < 3:
+            additional_money = get_additional_money()
+            customer_money += additional_money
+            if customer_money >= items[choice]:
+                print(f"Here's your {choice}!")
+                customer_money -= items[choice]
+                return customer_money, additional_money
+            else:
+                attempts += 1
+        else:
+            raise InsufficientFundsError("You do not have enough money for this item. Goodbye!")
+
+
+def shop():
+    items = {"bread": 50, "chips": 80, "milk": 120}
+    customer_money = 100
+
+    print(colored('Welcome to our shop!', 'cyan'))
+    while True:
+        display_items(items)
+        try:
+            choice = get_user_choice(items)
+            customer_money, additional_money = buy_item(choice, customer_money, items)
+        except ValueError as ve:
+            print(ve)
+        except InsufficientFundsError as ife:
+            print(ife)
+            break
+        else:
+            print(f"You have £{customer_money} left.")
+
+
+if __name__ == '__main__':
+    shop()
